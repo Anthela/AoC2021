0bff6ec6bf0f3ed13da6671bd68d553bba078e72
Anthela <nwwyf7@gmail.com>
2021-12-08 11:42:58 +0100

    Problem 8 solved

--- Problems/Model/Problem8/Digits.cs
+++ Problems/Model/Problem8/Digits.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode.Problems.Model.Problem8
+﻿namespace AdventOfCode.Problems.Model.Problem8
 {
     public class Digits
     {
@@ -15,5 +9,54 @@ namespace AdventOfCode.Problems.Model.Problem8
         public char BottomLeft { get; set; } = '*';
         public char BottomRight { get; set; } = '*';
         public char Bottom { get; set; } = '*';
+
+
+        public int EvaluateValue(string[] pattern)
+        {
+            double result = 0;
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                var multiplier = Math.Pow(10, 3 - i);
+
+                if (pattern[i].Length == 2)
+                    result += 1 * multiplier;
+
+                if (pattern[i].Length == 3)
+                    result += 7 * multiplier;
+
+                if (pattern[i].Length == 4)
+                    result += 4 * multiplier;
+
+                if (pattern[i].Length == 7)
+                    result += 8 * multiplier;
+
+                if (pattern[i].Length == 5)
+                {
+                    if (pattern[i].Any(x => x == Top) && pattern[i].Any(x => x == TopRight) && pattern[i].Any(x => x == Mid) && pattern[i].Any(x => x == BottomLeft) && pattern[i].Any(x => x == Bottom))
+                        result += 2 * multiplier;
+
+                    if (pattern[i].Any(x => x == Top) && pattern[i].Any(x => x == TopRight) && pattern[i].Any(x => x == Mid) && pattern[i].Any(x => x == BottomRight) && pattern[i].Any(x => x == Bottom))
+                        result += 3 * multiplier;
+
+                    if (pattern[i].Any(x => x == Top) && pattern[i].Any(x => x == TopLeft) && pattern[i].Any(x => x == Mid) && pattern[i].Any(x => x == BottomRight) && pattern[i].Any(x => x == Bottom))
+                        result += 5 * multiplier;
+                }
+
+                if (pattern[i].Length == 6)
+                {
+                    if (pattern[i].Any(x => x == Top) && pattern[i].Any(x => x == TopLeft) && pattern[i].Any(x => x == TopRight) && pattern[i].Any(x => x == BottomLeft) && pattern[i].Any(x => x == BottomRight) && pattern[i].Any(x => x == Bottom))
+                        result += 0 * multiplier;
+
+                    if (pattern[i].Any(x => x == Top) && pattern[i].Any(x => x == TopLeft) && pattern[i].Any(x => x == Mid) && pattern[i].Any(x => x == BottomLeft) && pattern[i].Any(x => x == BottomRight) && pattern[i].Any(x => x == Bottom))
+                        result += 6 * multiplier;
+
+                    if (pattern[i].Any(x => x == Top) && pattern[i].Any(x => x == TopLeft) && pattern[i].Any(x => x == TopRight) && pattern[i].Any(x => x == Mid) && pattern[i].Any(x => x == BottomRight) && pattern[i].Any(x => x == Bottom))
+                        result += 9 * multiplier;
+                }
+            }
+
+            return Convert.ToInt32(result);
+        }
     }
 }
--- Problems/Problem8.cs
+++ Problems/Problem8.cs
@@ -40,124 +40,46 @@ namespace AdventOfCode.Problems
 
         public int DoPartB()
         {
-            var lines = Utils.InputToStringArray("8_mini");
+            var lines = Utils.InputToStringArray("8");
 
-            // vonal után 2, 3, 4, 7 hosszúak számolása
-            // 2 hosszú - 1
-            // 3 hosszú - 7
-            // 4 hosszú - 4
-            // 7 hosszú - 8
+            Dictionary<string, string> beforeAndAfterLine = new();
 
-            /*
-                ****
-                *  *
-                *  *
-                ****
-                *  *
-                *  *
-                ****
+            char[] inputs = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
 
-            */
-
-
-
-            List<string> afterLine = new List<string>();
-            List<string> beforeLine = new List<string>();
-            List<string> sevenDigits = new();
-
-
-            List<Digits> codedDigits = new();
-
+            var sum = 0;
 
             foreach (var item in lines)
             {
                 var line = item.Substring(0, item.IndexOf('|') - 1);
-                beforeLine.Add(line); 
+                var afterLineString = item.Substring(item.IndexOf('|') + 2);
+
+                beforeAndAfterLine.Add(line, afterLineString);
             }
 
-            char[] inputs = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g'};
-
-            foreach (var item in beforeLine)
+            foreach (var item in beforeAndAfterLine)
             {
-                var digits = item.Split(' ');
+                var digits = item.Key.Split(' ');
                 Digits digit = new Digits();
 
-                var one = digits.Where(x => x.Length == 2).Single(); // topright, bottomright
-                var seven = digits.Where(x => x.Length == 3).Single(); // top
-                var four = digits.Where(x => x.Length == 4).Single(); // topleft, mid
+                var one = digits.Where(x => x.Length == 2).Single();
+                var seven = digits.Where(x => x.Length == 3).Single();
+                var four = digits.Where(x => x.Length == 4).Single();
+                var three = digits.Where(x => x.Length == 5 && x.Contains(one[0]) && x.Contains(one[1])).Single();
+                var five = digits.Where(x => x.Length == 5 && x != three && x.ToArray().Intersect(four.ToArray()).ToList().Count == 3).Single();
+                var two = digits.Where(x => x.Length == 5 && x != three && x != five).Single();
 
-                digit.TopRight = one[0];
-                digit.BottomRight = one[1];
                 digit.Top = seven.Where(x => x != one[0] && x != one[1]).Single();
-                digit.TopLeft = four.First(x => x != digit.TopRight && x != digit.BottomRight && x != digit.Top);
-                digit.Mid = four.Single(x => x != digit.TopRight && x != digit.BottomRight && x != digit.Top && x != digit.TopLeft);
+                digit.Mid = four.Intersect(five).Intersect(two).Single();
+                digit.TopLeft = four.Where(x => x != digit.Mid && !one.Contains(x)).Single();
+                digit.Bottom = two.Intersect(five).Where(x => x != digit.Top && x != digit.Mid).Single();
+                digit.BottomRight = five.Where(x => x != digit.Top && x != digit.TopLeft && x != digit.Mid && x != digit.Bottom).Single();
+                digit.TopRight = one.Where(x => x != digit.BottomRight).Single();
+                digit.BottomLeft = inputs.Where(x => x != digit.Top && x != digit.TopLeft && x != digit.TopRight && x != digit.Mid && x != digit.BottomRight && x != digit.Bottom).Single();
 
-                var notUsedInputs = inputs.Where(x => x != digit.TopRight && x != digit.BottomRight && x != digit.Top && x != digit.TopLeft && x != digit.Mid).ToArray();
-
-
-                // TODO
-
-
-                codedDigits.Add(digit);
+                sum += digit.EvaluateValue(item.Value.Split(' '));
             }
 
-
-            foreach (var item in codedDigits)
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    Console.Write(item.Top);
-                }
-                Console.WriteLine();
-
-                Console.Write(item.TopLeft + "   ");
-                Console.WriteLine(item.TopRight);
-                Console.Write(item.TopLeft + "   ");
-                Console.WriteLine(item.TopRight);
-                
-                for (int i = 0; i < 4; i++)
-                {
-                    Console.Write(item.Mid);
-                }
-                Console.WriteLine();
-
-                Console.Write(item.BottomLeft + "   ");
-                Console.WriteLine(item.BottomRight);
-                Console.Write(item.BottomLeft + "   ");
-                Console.WriteLine(item.BottomRight);
-
-                for (int i = 0; i < 4; i++)
-                {
-                    Console.Write(item.Bottom);
-                }
-                Console.WriteLine();
-
-                Console.WriteLine("----------------------------------------------");
-
-            }
-
-
-            /*
-            foreach (var item in lines)
-            {
-                var line = item.Substring(item.IndexOf('|') + 2);
-                afterLine.Add(line);
-            }
-
-            var sum = 0;
-
-            foreach (var item in afterLine)
-            {
-                var digits = item.Split(' ');
-
-                foreach (var digit in digits)
-                {
-                    if (digit.Length == 2 || digit.Length == 3 || digit.Length == 4 || digit.Length == 7)
-                        sum++;
-                }
-            }
-            */
-            return 0;
+            return sum;
         }
     }
 }
