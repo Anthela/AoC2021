fe44ce3e022be62ab4891c775a66043770c17232
Anthela <nwwyf7@gmail.com>
2021-12-08 11:45:46 +0100

    Preparing for Day 9

---
+++ Problems/Problem9.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Problems
+{
+    public class Problem9 : IProblem<int, int>
+    {
+        public int DoPartA()
+        {
+            var lines = Utils.InputToStringArray("8");
+
+            return 0;
+        }
+
+        public int DoPartB()
+        {
+            var lines = Utils.InputToStringArray("8");
+
+            return 0;
+        }
+    }
+}
