e57ab6964743257b6f45ee5b2f0d24a0f661f79a
Anthela <nwwyf7@gmail.com>
2021-12-09 15:21:30 +0100

    Problem 9 refact

--- Problems/Problem9.cs
+++ Problems/Problem9.cs
@@ -45,17 +45,17 @@ namespace AdventOfCode.Problems
 
             var table = CreateTableFromInput(lines);
 
-            var lowPoints = table.Where(x => x.Value.Marked).ToArray();
+            var lowPoints = table.Where(x => x.Value.Item2).ToArray();
 
-            List<KeyValuePair<(int, int), TableItem>> usedItems = new();
+            List<((int, int), int)> usedItems = new();
 
             List<int> result = new();
 
             foreach (var item in lowPoints)
             {
-                List<KeyValuePair<(int, int), TableItem>> currentLowPoints = new();
+                List<((int, int), int)> currentLowPoints = new();
 
-                currentLowPoints.Add(item);
+                currentLowPoints.Add((item.Key, item.Value.Item1));
 
                 usedItems.Clear();
 
@@ -65,43 +65,43 @@ namespace AdventOfCode.Problems
                     usedItems.Add(lowPoint);
 
                     // Left
-                    if (lowPoint.Key.Item2 - 1 >= 0)
+                    if (lowPoint.Item1.Item2 - 1 >= 0)
                     {
-                        var position = (lowPoint.Key.Item1, lowPoint.Key.Item2 - 1);
+                        var position = (lowPoint.Item1.Item1, lowPoint.Item1.Item2 - 1);
                         var leftItem = table[position];
 
-                        if (leftItem.Value != 9 && leftItem.Value >= lowPoint.Value.Value)
-                            currentLowPoints.Add(new KeyValuePair<(int, int), TableItem>(position, leftItem));
+                        if (leftItem.Item1 != 9 && leftItem.Item1 >= lowPoint.Item2)
+                            currentLowPoints.Add((position, leftItem.Item1));
                     }
 
                     // Right
-                    if (lowPoint.Key.Item2 + 1 < lines[0].Length)
+                    if (lowPoint.Item1.Item2 + 1 < lines[0].Length)
                     {
-                        var position = (lowPoint.Key.Item1, lowPoint.Key.Item2 + 1);
+                        var position = (lowPoint.Item1.Item1, lowPoint.Item1.Item2 + 1);
                         var rightItem = table[position];
 
-                        if (rightItem.Value != 9 && rightItem.Value >= lowPoint.Value.Value)
-                            currentLowPoints.Add(new KeyValuePair<(int, int), TableItem>(position, rightItem));
+                        if (rightItem.Item1 != 9 && rightItem.Item1 >= lowPoint.Item2)
+                            currentLowPoints.Add((position, rightItem.Item1));
                     }
 
                     // Top
-                    if (lowPoint.Key.Item1 - 1 >= 0)
+                    if (lowPoint.Item1.Item1 - 1 >= 0)
                     {
-                        var position = (lowPoint.Key.Item1 - 1, lowPoint.Key.Item2);
+                        var position = (lowPoint.Item1.Item1 - 1, lowPoint.Item1.Item2);
                         var topItem = table[position];
 
-                        if (topItem.Value != 9 && topItem.Value >= lowPoint.Value.Value)
-                            currentLowPoints.Add(new KeyValuePair<(int, int), TableItem>(position, topItem));
+                        if (topItem.Item1 != 9 && topItem.Item1 >= lowPoint.Item2)
+                            currentLowPoints.Add((position, topItem.Item1));
                     }
 
                     // Bottom
-                    if (lowPoint.Key.Item1 + 1 < lines.Length)
+                    if (lowPoint.Item1.Item1 + 1 < lines.Length)
                     {
-                        var position = (lowPoint.Key.Item1 + 1, lowPoint.Key.Item2);
+                        var position = (lowPoint.Item1.Item1 + 1, lowPoint.Item1.Item2);
                         var bottomItem = table[position];
 
-                        if (bottomItem.Value != 9 && bottomItem.Value >= lowPoint.Value.Value)
-                            currentLowPoints.Add(new KeyValuePair<(int, int), TableItem>(position, bottomItem));
+                        if (bottomItem.Item1 != 9 && bottomItem.Item1 >= lowPoint.Item2)
+                            currentLowPoints.Add((position, bottomItem.Item1));
                     }
 
                     currentLowPoints = currentLowPoints.Except(usedItems).ToList();
@@ -113,9 +113,9 @@ namespace AdventOfCode.Problems
             return result.OrderByDescending(i => i).Take(3).Aggregate(1, (a, b) => a *= b);
         }
 
-        private Dictionary<(int, int), TableItem> CreateTableFromInput(string[] lines)
+        private Dictionary<(int, int), (int, bool)> CreateTableFromInput(string[] lines)
         {
-            Dictionary<(int, int), TableItem> table = new();
+            Dictionary<(int, int), (int, bool)> table = new();
 
             for (int i = 0; i < lines.Length; i++)
             {
@@ -139,7 +139,7 @@ namespace AdventOfCode.Problems
                         if (j + 1 < lines[i].Length)
                             valid = Convert.ToInt32(lines[i][j + 1].ToString()) > number;
 
-                    table[(i, j)] = new TableItem() { Value = number, Marked = valid };
+                    table[(i, j)] = (number, valid);
                 }
             }
 
