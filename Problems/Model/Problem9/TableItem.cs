4813ce7fed3a94feb7f7a7dbe1f80646bc8a3863
Anthela <nwwyf7@gmail.com>
2021-12-09 15:22:12 +0100

    Delete TableItem.cs

--- Problems/Model/Problem9/TableItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdventOfCode_2021.Problems.Model.Problem9
-{
-    public class TableItem
-    {
-        public int Value { get; set; }
-        public bool Marked { get; set; }
-    }
-}
