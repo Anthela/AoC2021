4afb91963aae2b57481d93c8df12fac9218ee31e
Anthela <nwwyf7@gmail.com>
2021-12-15 09:53:04 +0100

    Problem 15 part1 solved

--- Problems/Problem15.cs
+++ Problems/Problem15.cs
@@ -4,14 +4,63 @@
     {
         public int DoPartA()
         {
-            var lines = Utils.InputToStringArray("15_mini").ToArray();
+            var lines = Utils.InputToStringArray("15").ToArray();
 
-            return 0;
+            Dictionary<(int, int), int> table = new();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    table[(i, j)] = Convert.ToInt32(lines[i][j].ToString());
+                }
+            }
+
+            var bottomRight = (table.Keys.MaxBy(x => x.Item1).Item1, table.Keys.MaxBy(x => x.Item2).Item2);
+
+            var q = new PriorityQueue<(int, int), int>();
+            var totalRisk = new Dictionary<(int, int), int>();
+
+            totalRisk[(0, 0)] = 0;
+            q.Enqueue((0, 0), 0);
+
+            while (q.Count > 0)
+            {
+                var position = q.Dequeue();
+
+                foreach (var neighbour in Neighbours(position))
+                {
+                    if (table.ContainsKey(neighbour) && !totalRisk.ContainsKey(neighbour))
+                    {
+                        var risk = totalRisk[position] + table[neighbour];
+
+                        totalRisk[neighbour] = risk;
+
+                        if (neighbour == bottomRight)
+                        {
+                            q.Clear();
+                            break;
+                        }
+
+                        q.Enqueue(neighbour, risk);
+                    }
+                }
+            }
+
+            return totalRisk[bottomRight];
         }
 
         public int DoPartB()
         {
             return 0;
+        }
+
+        private IEnumerable<(int, int)> Neighbours((int, int) position)
+        {
+            yield return (position.Item1 - 1, position.Item2);
+            yield return (position.Item1 + 1, position.Item2);
+            yield return (position.Item1, position.Item2 - 1);
+            yield return (position.Item1, position.Item2 + 1);
         }
     }
 }
