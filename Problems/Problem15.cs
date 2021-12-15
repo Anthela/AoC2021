a007c772abcf7524d74e779cafb232ea5be8f8a6
Anthela <nwwyf7@gmail.com>
2021-12-15 19:32:04 +0100

    Problem 15 solved

--- Problems/Problem15.cs
+++ Problems/Problem15.cs
@@ -16,6 +16,58 @@
                 }
             }
 
+            return CalculateLowestRiskPath(table);
+        }
+
+        public int DoPartB()
+        {
+            var lines = Utils.InputToStringArray("15").ToArray();
+
+            Dictionary<(int, int), int> table = new();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    int value = Convert.ToInt32(lines[i][j].ToString());
+
+                    table[(i, j)] = value;
+
+                    for (int k = 1; k < 5; k++)
+                    {
+                        table[(i, j + k * lines[i].Length)] = value + k <= 9 ? value + k : value + k - 9;
+                    }
+                }
+            }
+
+            var bottomRight = (table.Keys.MaxBy(x => x.Item1).Item1, table.Keys.MaxBy(x => x.Item2).Item2);
+
+            for (int i = 0; i <= bottomRight.Item1; i++)
+            {
+                for (int j = 0; j <= bottomRight.Item2; j++)
+                {
+                    int value = table[(i, j)];
+
+                    for (int k = 1; k < 5; k++)
+                    {
+                        table[(i + k * (bottomRight.Item1 + 1), j)] = value + k <= 9 ? value + k : value + k - 9;
+                    }
+                }
+            }
+
+            return CalculateLowestRiskPath(table);
+        }
+
+        private IEnumerable<(int, int)> Neighbours((int, int) position)
+        {
+            yield return (position.Item1 - 1, position.Item2);
+            yield return (position.Item1 + 1, position.Item2);
+            yield return (position.Item1, position.Item2 - 1);
+            yield return (position.Item1, position.Item2 + 1);
+        }
+
+        private int CalculateLowestRiskPath(Dictionary<(int, int), int> table)
+        {
             var bottomRight = (table.Keys.MaxBy(x => x.Item1).Item1, table.Keys.MaxBy(x => x.Item2).Item2);
 
             var q = new PriorityQueue<(int, int), int>();
@@ -48,19 +100,6 @@
             }
 
             return totalRisk[bottomRight];
-        }
-
-        public int DoPartB()
-        {
-            return 0;
-        }
-
-        private IEnumerable<(int, int)> Neighbours((int, int) position)
-        {
-            yield return (position.Item1 - 1, position.Item2);
-            yield return (position.Item1 + 1, position.Item2);
-            yield return (position.Item1, position.Item2 - 1);
-            yield return (position.Item1, position.Item2 + 1);
         }
     }
 }
---
+++ Problems/Problem16.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode_2021.Problems
+{
+    public class Problem16 : IProblem<int, int>
+    {
+        public int DoPartA()
+        {
+            var lines = Utils.InputToStringArray("16_mini").ToArray();
+
+            return 0;
+        }
+
+        public int DoPartB()
+        {
+            return 0;
+        }
+    }
+}
