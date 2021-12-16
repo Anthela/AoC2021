e1d56bc7974c8a3c7ad9e8de2662f5a52fcdb264
Anthela <nwwyf7@gmail.com>
2021-12-16 21:42:54 +0100

    Problem 16 part1 solved

--- Problems/Problem16.cs
+++ Problems/Problem16.cs
@@ -4,7 +4,7 @@
     {
         public int DoPartA()
         {
-            var line = Utils.InputToStringArray("16_mini").Single();
+            var line = Utils.InputToStringArray("16").Single();
 
             string binarystring = string.Join(string.Empty, line.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
 
@@ -20,36 +20,47 @@
             // 110 000 0 000000001010100 000 000 0 000000000010110 000 100 01010 110 100 01011 100 000 1 00000000010 111 100 01100 000 100 01101 000000
             //  V6   0 0              84  V0   0 0              22  V0   4    10  V6   4    11  V4   0 1           2  V7   4    12  V0  4     13 ------        
 
+            // A0016C880162017C3686B18A3D4780
+            // 101 000 0 000000001011011 001 000 1 00000000001 011 000 1 00000000101 111 100 00110 110 100 00110 101 100 01100 010 100 01111 010 100 01111 0000000
+            //  V5                        V1                    V3                    V7            V6            V5            V2            V2           -------
 
-            var version = Convert.ToInt32(binarystring.Substring(0, 3), 2);
-            var typeId = Convert.ToInt32(binarystring.Substring(3, 3), 2);
-
-            if (typeId == 4)
-            {
-
-            }
-            else
-            {
-                var lengthTypeId = Convert.ToInt32(binarystring.Substring(4, 1), 2);
-
-                if(lengthTypeId == 1)
-                {
-                    var numberOfPackets = Convert.ToInt32(binarystring.Substring(7, 11), 2);
-
-                }
-                else
-                {
-
-                }
-            }
-
-
-            return 0;
+            return ProcessBinaryString(binarystring);
         }
 
         public int DoPartB()
         {
             return 0;
+        }
+
+        private int ProcessBinaryString(string binarystring)
+        {
+            var version = 0;
+
+            if (binarystring.Length > 7)
+            {
+                version = Convert.ToInt32(binarystring.Substring(0, 3), 2);
+                var typeId = Convert.ToInt32(binarystring.Substring(3, 3), 2);
+
+                if (typeId == 4)
+                {
+                    var zeroPosition = 6;
+
+                    while (Convert.ToInt32(binarystring.Substring(zeroPosition, 1), 2) != 0)
+                        zeroPosition += 5;
+
+                    zeroPosition += 5;
+
+                    version += ProcessBinaryString(binarystring.Substring(zeroPosition));
+                }
+                else
+                {
+                    var lengthTypeId = Convert.ToInt32(binarystring.Substring(6, 1), 2);
+
+                    version += ProcessBinaryString(binarystring.Substring(lengthTypeId == 1 ? 18 : 22));
+                }
+            }
+
+            return version;
         }
     }
 }
