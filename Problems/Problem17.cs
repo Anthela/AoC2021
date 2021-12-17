27c2d9a0fbf0e653a828c29f72afe765fd08acaa
Anthela <nwwyf7@gmail.com>
2021-12-17 08:55:29 +0100

    Problem 17 part1 solved

---
+++ Problems/Problem17.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode_2021.Problems
+{
+    public class Problem17 : IProblem<int, int>
+    {
+        public int DoPartA()
+        {
+            var y = Math.Abs(Utils.InputToStringArray("17").Select(line => Convert.ToInt32(line.Split(", ").Last().Split('=').Last().Split("..").First())).Single());
+
+            return (int)((y - 1) / 2.0 * y);
+        }
+
+        public int DoPartB()
+        {
+            return 0;
+        }
+    }
+}
