06c4f2a2446775df47f1848dca89d0f8ad06fb47
Anthela <nwwyf7@gmail.com>
2021-12-18 10:04:17 +0100

    Problem 17 part2 solved

--- Problems/Problem17.cs
+++ Problems/Problem17.cs
@@ -11,7 +11,59 @@
 
         public int DoPartB()
         {
-            return 0;
+            var targetArea = Utils.InputToStringArray("17").Select(line => line.Split(": ").Last()).Single().Split(", ");
+
+            var xRange = targetArea.First().Split('=').Last().Split("..").Select(coords => Convert.ToInt32(coords));
+            var yRange = targetArea.Last().Split('=').Last().Split("..").Select(coords => Convert.ToInt32(coords));
+            var left = xRange.First();
+            var right = xRange.Last();
+            var bottom = yRange.First();
+            var top = yRange.Last();
+
+            var maxX = right;
+            var minY = bottom;
+            var maxY = Math.Abs(bottom) - 1;
+
+            var minX = 1;
+
+            while (minX / 2.0 * (minX + 1) < left)
+                minX++;
+
+            var result = 0;
+
+            for (int i = minY; i <= maxY; i++)
+            {
+                for (int j = minX; j <= maxX; j++)
+                {
+                    if (CanIShootIntoTheTargetedArea(j, i, left, right, bottom, top))
+                        result++;
+                }
+            }
+
+            return result;
+        }
+
+        private bool CanIShootIntoTheTargetedArea(int x, int y, int left, int right, int bottom, int top)
+        {
+            bool bingo = false;
+
+            var posX = 0;
+            var posY = 0;
+
+            while (posX <= right && posY >= bottom && !bingo)
+            {
+                posX += x;
+                posY += y--;
+
+                if (posX >= left && posX <= right && posY >= bottom && posY <= top)
+                    bingo = true;
+
+                if (!bingo)
+                    if (x > 0) x--;
+                    else if (x < 0) x++;
+            }
+
+            return bingo;
         }
     }
 }
